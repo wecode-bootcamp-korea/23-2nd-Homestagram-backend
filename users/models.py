0b2a1423498f5430b9b07e835d1aaa1bbfad0ab6
JINSU <songjinsu@MacBook-Pro.local>
2021-08-20 18:21:08 +0900

    Modify: User 모델 nickname 필드 수정

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    nickname    = models.CharField(max_length=20)
+    nickname    = models.CharField(max_length=20, null=True)
     kakao_id    = models.BigIntegerField()
     kakao_email = models.CharField(max_length=50)
 
