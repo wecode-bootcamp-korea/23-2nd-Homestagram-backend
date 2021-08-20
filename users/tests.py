6d10d12b9d475e30701ed2368c28e70442c3c72d
JINSU <songjinsu@MacBook-Pro.local>
2021-08-20 13:26:02 +0900

    Add: SignUpView

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,170 @@
-from django.test import TestCase
+from unittest.mock import patch, MagicMock
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from users.models import User
+
+class SignUpTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id          = 1,
+            nickname    = "test",
+            kakao_id    = 1111111111,
+            kakao_email = "test@test.com",
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    @patch("users.views.requests")
+    def test_signup_new_user_success(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "id": 2222222222,
+                    'connected_at': '2021-08-19T07:59:52Z', 
+                    'properties': {'nickname': 'testtest'}, 
+                    'kakao_account': {
+                        'profile_nickname_needs_agreement': False,
+                        'profile': {'nickname': 'testtest'},
+                        'has_email': True,
+                        'email_needs_agreement': False,
+                        'is_email_valid': True,
+                        'is_email_verified': True,
+                        'email': 'test2@test.com'
+                    }
+                }
+
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+
+        data = {
+            'access_token' : 'fake_access_token',
+            'nickname'     : 'test2' 
+        }
+
+        response = client.post('/users/signup', content_type='application/json', data=data)
+
+        self.assertEqual(response.status_code, 201)
+
+    @patch("users.views.requests")
+    def test_signup_invalid_kakao_token(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "msg": "this access token does not exist",
+                    "code": -401
+                }
+
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+
+        data = {
+            'access_token' : 'fake_access_token',
+            'nickname'     : 'test2' 
+        }
+
+        response = client.post('/users/signup', content_type='application/json', data=data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'MESSAGE':'INVALID_KAKAO_TOKEN'})
+
+    @patch("users.views.requests")
+    def test_signup_need_email_agreement(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "id": 2222222222,
+                    'connected_at': '2021-08-19T07:59:52Z', 
+                    'properties': {'nickname': 'testtest'}, 
+                    'kakao_account': {
+                        'profile_nickname_needs_agreement': False,
+                        'profile': {'nickname': 'testtest'},
+                        'has_email': True,
+                        'email_needs_agreement': True,
+                        'is_email_valid': True,
+                        'is_email_verified': True,
+                    }
+                }
+
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+
+        data = {
+            'access_token' : 'fake_access_token',
+            'nickname'     : 'test2' 
+        }
+
+        response = client.post('/users/signup', content_type='application/json', data=data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'MESSAGE':'NEED_EMAIL_AGREEMENT'})
+
+    @patch("users.views.requests")
+    def test_signup_nickname_already_exist(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "id": 2222222222,
+                    'connected_at': '2021-08-19T07:59:52Z', 
+                    'properties': {'nickname': 'testtest'}, 
+                    'kakao_account': {
+                        'profile_nickname_needs_agreement': False,
+                        'profile': {'nickname': 'testtest'},
+                        'has_email': True,
+                        'email_needs_agreement': False,
+                        'is_email_valid': True,
+                        'is_email_verified': True,
+                        'email': 'test2@test.com'
+                    }
+                }
+
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+
+        data = {
+            'access_token' : 'fake_access_token',
+            'nickname'     : 'test' 
+        }
+
+        response = client.post('/users/signup', content_type='application/json', data=data)
+
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(response.json(), {'MESSAGE':'NICKNAME_ALREADY_EXISTS'})
+
+    @patch("users.views.requests")
+    def test_signup_token_key_error(self, mocked_requests):
+        client = Client()
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    "id": 2222222222,
+                    'connected_at': '2021-08-19T07:59:52Z', 
+                    'properties': {'nickname': 'testtest'}, 
+                    'kakao_account': {
+                        'profile_nickname_needs_agreement': False,
+                        'profile': {'nickname': 'testtest'},
+                        'has_email': True,
+                        'email_needs_agreement': False,
+                        'is_email_valid': True,
+                        'is_email_verified': True,
+                        'email': 'test2@test.com'
+                    }
+                }
+
+        mocked_requests.get = MagicMock(return_value = MockedResponse())
+
+        data = {
+            'access_token22222' : 'fake_access_token',
+            'nickname'          : 'test' 
+        }
+
+        response = client.post('/users/signup', content_type='application/json', data=data)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'MESSAGE':'KEY_ERROR'})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+import json, jwt, requests
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from users.models import User
+from my_settings  import SECRET_KEY
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            kakao_access_token = data['access_token']
+            kakao_user_info = requests.get('https://kapi.kakao.com/v2/user/me', headers={'Authorization':f'Bearer {kakao_access_token}'}).json()
+
+            kakao_id = kakao_user_info.get('id', None)
+
+            if not kakao_id:
+                return JsonResponse({'MESSAGE':'INVALID_KAKAO_TOKEN'}, status=400)
+
+            kakao_email = kakao_user_info['kakao_account'].get('email', None)
+
+            if not kakao_email:
+                if kakao_user_info['kakao_account']['email_needs_agreement']:
+                    return JsonResponse({'MESSAGE':'NEED_EMAIL_AGREEMENT'}, status=400)
+
+                return JsonResponse({'MESSAGE':'INVALID_KAKAO_TOKEN'}, status=400)
+
+            if User.objects.filter(nickname=data['nickname']).exists():
+                return JsonResponse({'MESSAGE':'NICKNAME_ALREADY_EXISTS'}, status=409)
+
+            User.objects.create(
+                kakao_id    = kakao_user_info['id'],
+                kakao_email = kakao_user_info['kakao_account']['email'],
+                nickname    = data['nickname'],
+            )
+
+            return JsonResponse({'MESSAGE':'SIGN_UP_SUCCESS'}, status=201)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
