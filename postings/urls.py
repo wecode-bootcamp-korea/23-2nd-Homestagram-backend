1603b988a13c28ae547e0017442fdb9a22377f76
jun <wkdghwns91@gmail.com>
2021-08-22 15:49:17 +0900

    [23기 장호준] ADD : 북마크 기능

--- postings/urls.py
+++ postings/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from postings.views import PostingView
+from postings.views import PostingView, BookmarkView
 
 urlpatterns = [
-    path('', PostingView.as_view())
+    path('', PostingView.as_view()),
+    path('/<int:posting_id>/bookmark', BookmarkView.as_view()),
+    path('/<int:user_id>/bookmarks', BookmarkView.as_view())
 ]
