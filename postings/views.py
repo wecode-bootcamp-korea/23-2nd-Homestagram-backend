9272c412db60e6c35681a7085ff2bcd7438670bd
jun <wkdghwns91@gmail.com>
2021-08-24 13:47:04 +0900

    [23기 장호준] Modify : 게시글 등록 - 게시글 등록 시에 태그 정보도 함께 등록되는 기능 - 유닛 테스트 완료

--- postings/views.py
+++ postings/views.py
@@ -1,51 +1,69 @@
-import boto3, uuid
+import boto3, uuid, json
 
 from django.http  import JsonResponse
 from django.views import View
+from django.db    import transaction
+from django.db.models import Max
 
-from postings.models          import Posting, DesignType
-from homestagram.settings     import AWS_STORAGE_BUCKET_NAME, AWS_S3_SECRET_ACCESS_KEY, AWS_S3_ACCESS_KEY_ID
+from postings.models          import Posting, DesignType, Tag
+from homestagram.settings     import AWS_STORAGE_BUCKET_NAME, AWS_S3_SECRET_ACCESS_KEY, AWS_S3_ACCESS_KEY_ID, S3_URL
 from users.utils              import SignInDecorator
 from users.models             import Bookmark, User
+from products.models          import Product
 
 class PostingView(View):
     @SignInDecorator
+    @transaction.atomic
     def post(self, request):
         try:
             content     = request.POST.get('content')
             design_type = request.POST.get('design_type')
-            image       = request.FILES.get('image')
-            
+            image       = request.FILES.get('file')
+            tags        = json.loads(request.POST.get('list'))['tags']
+            user        = request.user
+
             if not image:
                 return JsonResponse({'MESSAGE' : 'IMAGE_EMPTY'}, status=400)
 
-            upload_key = str(uuid.uuid4()) + image.name
-            s3_client  = boto3.client(
-                's3',
-                aws_access_key_id     = AWS_S3_ACCESS_KEY_ID,
-                aws_secret_access_key = AWS_S3_SECRET_ACCESS_KEY,
-            )
+            with transaction.atomic():
+                upload_key = str(uuid.uuid4()) + image.name
+                s3_client  = boto3.client(
+                    's3',
+                    aws_access_key_id     = AWS_S3_ACCESS_KEY_ID,
+                    aws_secret_access_key = AWS_S3_SECRET_ACCESS_KEY,
+                )
             
-            Posting.objects.create(
-                content     = content,
-                image_url   = 'https://homestagram.s3.ap-northeast-2.amazonaws.com/' + upload_key,
-                design_type = DesignType.objects.get(name=design_type),
-                user        = request.user
-            )
+                posting = Posting.objects.create(
+                    content     = content,
+                    image_url   = S3_URL + upload_key,
+                    design_type = DesignType.objects.get(name=design_type),
+                    user        = user
+                )
 
-            s3_client.upload_fileobj(
-                image,
-                AWS_STORAGE_BUCKET_NAME,
-                upload_key,
-                ExtraArgs = {
-                    'ContentType' : image.content_type
-                }
-            )
+                Tag.objects.bulk_create(
+                    Tag(
+                        coordinate = (tag['xx'], tag['yy']),
+                        posting = posting,
+                        product = Product.objects.get(id=tag['product_id'])
+                    ) for tag in tags
+                )
+
+                s3_client.upload_fileobj(
+                    image,
+                    AWS_STORAGE_BUCKET_NAME,
+                    upload_key,
+                    ExtraArgs = {
+                        'ContentType' : image.content_type
+                    }
+                )
 
             return JsonResponse({'MESSAGE' : 'POSTING_SUCCESS'}, status=200)
         
         except DesignType.DoesNotExist:
             return JsonResponse({'MESSAGE' : 'DESIGN_TYPE_DOES_NOT_EXIST'}, status=400)
+        
+        except KeyError:
+            return JsonResponse({'MESSAGE' : 'KEY_ERROR'}, status=400)
 
 class BookmarkView(View):
     @SignInDecorator
