22ec277f715d94a54aa1e75feb3b779184f5120a
JINSU <songjinsu@MacBook-Pro.local>
2021-08-24 19:22:16 +0900

    Add: FollowView GET method

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import SocialSignInView, NicknameView
+from users.views import SocialSignInView, NicknameView, FollowView
 
 urlpatterns = [
     path('/signin', SocialSignInView.as_view()),
     path('/<int:user_id>/nickname', NicknameView.as_view()),
+    path('/follow', FollowView.as_view())
 ]
